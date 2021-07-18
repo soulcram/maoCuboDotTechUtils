0af5d3f34218b35bf4bf19691a5b09e9c56082f4
soulcram <mpwmnw@gmail.com>
2021-07-17 21:46:44 -0300

    Atualizando utils

--- src/main/java/br/com/m3Tech/utils/StringUtils.java
+++ src/main/java/br/com/m3Tech/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package br.com.m3Tech.utils;
 
+import java.text.Normalizer;
+
 public class StringUtils {
 	
 	private StringUtils() {};
@@ -40,6 +42,36 @@ public class StringUtils {
 	public static boolean init(String valor) {
 	
 		return false;
+	}
+	
+	public static boolean EmptyOrNull(String valor) {
+		return valor == null || "".equals(valor);
+		
+	}
+
+	public static String limite(String nome, int i) {
+		if(nome.length() > i) {
+			return nome.substring(0, i -1);
+		}else {
+			return nome;
+		}
+	}
+
+	public static String tamFinal(String nome, int i) {
+		if(nome.length() > i) {
+			return nome.substring(0, i);
+		}else if(nome.length() < i) {
+			for(int t = nome.length(); t < i; t++ ) {
+				nome += " ";
+			}
+			return nome;
+		}else {
+			return nome;
+		}
+	}
+	
+	public static String removerAcentos(String value) {
+		return Normalizer.normalize(value, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
 	}
 
 }
