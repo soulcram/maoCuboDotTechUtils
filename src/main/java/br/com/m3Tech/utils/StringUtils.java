ea5722ad470640e73c041e1dae3693c4c103f575
soulcram <mpwmnw@gmail.com>
2022-03-21 07:13:03 -0300

    Adicionando novos utils vindo do AppLoja

--- src/main/java/br/com/m3Tech/utils/StringUtils.java
+++ src/main/java/br/com/m3Tech/utils/StringUtils.java
@@ -1,94 +1,106 @@
 package br.com.m3Tech.utils;
 
+import java.math.BigDecimal;
 import java.text.Normalizer;
+import java.text.NumberFormat;
 
 public class StringUtils {
-	
-	private StringUtils() {};
-	
+
+	private StringUtils() {
+	}
+
 	public static boolean isNotEmpty(String valor) {
-		
+
 		return !isEmpty(valor);
 	}
-	
+
 	public static boolean isEmpty(String valor) {
-		if(isNull(valor)) {
+		if (isNull(valor)) {
 			return true;
 		}
-		
-		if(isBlank(valor)) {
+
+		if (isBlank(valor)) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean isNull(String valor) {
-		if(valor == null) {
+		if (valor == null) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean isBlank(String valor) {
-		if("".equals(valor.trim())) {
+		if ("".equals(valor.trim())) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	public static boolean init(String valor) {
-	
+
 		return false;
 	}
-	
+
 	public static boolean emptyOrNull(String valor) {
 		return valor == null || "".equals(valor);
-		
+
 	}
 
 	public static String limite(String nome, int i) {
-		if(nome.length() > i) {
-			return nome.substring(0, i -1);
-		}else {
+		if (nome.length() > i) {
+			return nome.substring(0, i - 1);
+		} else {
 			return nome;
 		}
 	}
 
 	public static String tamFinal(String nome, int i) {
-		if(nome.length() > i) {
+		if (nome.length() > i) {
 			return nome.substring(0, i);
-		}else if(nome.length() < i) {
-			for(int t = nome.length(); t < i; t++ ) {
+		} else if (nome.length() < i) {
+			for (int t = nome.length(); t < i; t++) {
 				nome += " ";
 			}
 			return nome;
-		}else {
+		} else {
 			return nome;
 		}
 	}
-	
+
+	public static String getFormatoMoeda(BigDecimal valor) {
+		NumberFormat nf = NumberFormat.getCurrencyInstance();
+		return nf.format(valor);
+	}
+
 	public static String removerAcentos(String value) {
+
+		if (isNull(value)) {
+			return "";
+		}
+
 		return Normalizer.normalize(value, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
 	}
 
 	public static boolean containsOnly(String s1, String s2) {
-		
-		for(char c : s1.toCharArray()) {
+
+		for (char c : s1.toCharArray()) {
 			String s = String.valueOf(c);
-			if(!s2.contains(s)) {
+			if (!s2.contains(s)) {
 				return false;
 			}
 		}
-		
-		
+
 		return true;
 	}
 
 	public static boolean isNumeric(String s) {
-				return containsOnly(s, "0123456789");
+		return containsOnly(s, "0123456789");
 	}
 
 }
