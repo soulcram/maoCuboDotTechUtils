7714e7733964a36e5187f70e73a00dc4b599a9e1
marcos.wanderley <marcos.wanderley@sinqia.com.br>
2022-05-14 02:10:27 -0300

    boolean from string

--- src/main/java/br/com/m3Tech/utils/BooleanUtils.java
+++ src/main/java/br/com/m3Tech/utils/BooleanUtils.java
@@ -15,5 +15,25 @@ public class BooleanUtils {
 	public static Boolean defaultTrueIfNull(Boolean valor) {
 		return valor == null ? true : valor;
 	}
+	
+	public static Boolean getBooleanOfInteger(Integer valor) {
+		return valor == 1 ? true : false;
+	}
+	
+	public static Boolean getBooleanOfString(String valor) {
+		
+		if("s" .equalsIgnoreCase(valor) ||
+		   "true" .equalsIgnoreCase(valor) ||
+		   "sim" .equalsIgnoreCase(valor) ||
+		   "1" .equalsIgnoreCase(valor) ||
+		   "yes" .equalsIgnoreCase(valor) ||
+		   "y" .equalsIgnoreCase(valor) ) {
+			
+			return true;
+			
+		}else {
+			return false;
+		}
+	}
 
 }
